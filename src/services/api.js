e846e18e5881da3704ac33aa3367ba95435ff5f4
Larissa Campelo Alves <larissacampeloalves@gmail.com>
2021-08-29 21:46:22 -0300

    Coloca url do Backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://backend-jobsnet-2021.herokuapp.com',
 })
 
 export default api;
